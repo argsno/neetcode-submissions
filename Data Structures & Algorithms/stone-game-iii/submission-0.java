11123b6c906c4c2b06a27a18f6472e56dcfd6bae
David Wong <argsno@gmail.com>
2026-08-28 23:08:55 +0800

    Add: stone-game-iii - submission-0

---
+++ Data Structures & Algorithms/stone-game-iii/submission-0.java
@@ -0,0 +1,22 @@
+class Solution {
+    public String stoneGameIII(int[] stoneValue) {
+        int n = stoneValue.length;
+        int[] dp = new int[n+1];
+        dp[n] = 0;
+        for (int i = n-1; i >= 0; i--) {
+            dp[i] = Integer.MIN_VALUE;
+            int curSum = 0;
+            for (int k = 1; k <= 3 && i+k-1 < n; k++) {
+                curSum += stoneValue[i+k-1];
+                dp[i] = Math.max(dp[i], curSum - dp[i+k]);
+            }
+        }
+        if (dp[0] > 0) {
+            return "Alice";
+        } else if (dp[0] < 0) {
+            return "Bob";
+        } else {
+            return "Tie";
+        }
+    }
+}
