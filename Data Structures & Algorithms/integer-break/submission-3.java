a6e3ce7e63cee34bc58d26571632e32f76a95872
David Wong <argsno@gmail.com>
2026-08-28 17:45:52 +0800

    Add: integer-break - submission-3

---
+++ Data Structures & Algorithms/integer-break/submission-3.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int integerBreak(int n) {
+        if (n == 2) return 1;
+        if (n == 3) return 2;
+        int[] dp = new int[3];
+        dp[0] = 1;
+        dp[1] = 1;
+        dp[2] = 2;
+        for (int i = 3; i <= n; i++) {
+            int curr = Math.max(dp[1]*2, dp[0]*3);
+            dp[0] = dp[1];
+            dp[1] = dp[2];
+            dp[2] = curr;
+        }
+        return dp[2];
+    }
+}
